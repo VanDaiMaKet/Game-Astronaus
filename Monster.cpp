a56b02574f7e6a2e2b7fa79802d84605d0d4e3d6
VanDaiMaKet <160533381+VanDaiMaKet@users.noreply.github.com>
2024-03-29 15:51:20 +0700

    Add files via upload

---
+++ Monster.cpp
@@ -0,0 +1,152 @@
+﻿#include "Monster.h"
+Monster::Monster() {
+	frame = 0;
+	x_pos = 0.0;
+	y_pos = 0.0;
+	x_change = 0.0;
+	y_change = 0.0;
+	w_frame = 0;
+	h_frame = 0;
+	onGround = false;
+	map_x = 0;
+	map_y = 0;
+	move_limit_a = 0;
+	move_limit_b = 0;
+	input_type.left = 1;
+	type_move = MONSTER;
+
+}
+Monster::~Monster() {
+	Free();
+}
+bool Monster::LoadImg(string path, SDL_Renderer* des) {
+	bool load = BaseObject::LoadImg(path, des);
+	if (load = true)
+	{
+		w_frame = rect_.w / 5;
+		h_frame = rect_.h;
+	}
+	return load;
+}
+void Monster::LoadFrame() {
+	if (w_frame > 0 && h_frame > 0)
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			frame_clip[i].x = i * w_frame;
+			frame_clip[i].y = 0;
+			frame_clip[i].w = w_frame;
+			frame_clip[i].h = h_frame;
+		}
+	}
+}
+void Monster::Show(SDL_Renderer* des) {
+	rect_.x = x_pos - map_x;// vị trí xuất hiện trên màn hình
+	rect_.y = y_pos - map_y + 6;// vị trí xuất hiện trên màn hình
+	frame++;
+	if (frame / 50 >= 5) frame = 0;
+	SDL_Rect* currentClip = &frame_clip[frame / 50];//nguồn ảnh
+	SDL_Rect renderQuad = { rect_.x, rect_.y, w_frame, h_frame };// điểm xuất hiện trên màn hình
+	SDL_RenderCopy(des, object_, currentClip, &renderQuad);
+}
+
+void Monster::Update(Map& map) {
+	x_change = 0;
+	y_change += MONSTER_FALLING_SPEED;
+	if (y_change >= MAX_MONSTER_FALLING_SPEED) {
+		y_change = MAX_MONSTER_FALLING_SPEED;
+	}
+	if (input_type.left == 1) {
+		x_change -= MONSTER_SPEED;
+	}
+	else if (input_type.right == 1) {
+		x_change += MONSTER_SPEED;
+	}
+
+
+	MapCollisionCheck(map);
+
+}
+
+void Monster::MapCollisionCheck(Map& map) {
+	int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+	// kiểm tra theo chiều ngang
+	int heightMin = (h_frame < TILE_SIZE ? h_frame : TILE_SIZE) - 30;
+	x1 = (x_pos + x_change) / TILE_SIZE;//tìm nhân vật ở ô thứ bao nhiêu
+	x2 = (x_pos + x_change + w_frame - 1) / TILE_SIZE;
+	y1 = (y_pos + 5) / TILE_SIZE;
+	y2 = (y_pos + heightMin - 1) / TILE_SIZE;
+	if (x1 >= 0 && x2 <= MAX_MAP_WIDTH && y1 >= 0 && y2 <= MAX_MAP_HEIGHT) {
+		if (x_change > 0) {//tiến
+			if (map.tile[y1][x2] == 1 || map.tile[y1][x2] == 2 || map.tile[y1][x2] == 3 || map.tile[y1][x2] == 6 || map.tile[y1][x2] == 7 || map.tile[y2][x2] == 1 || map.tile[y2][x2] == 2 || map.tile[y2][x2] == 3 || map.tile[y2][x2] == 6 || map.tile[y2][x2] == 7) {
+				x_pos = x2 * TILE_SIZE;
+				x_pos -= w_frame + 1;
+				x_change = 0;
+			}
+		}
+		else if (x_change < 0) {//lùi
+			if (map.tile[y1][x1] == 1 || map.tile[y1][x1] == 2 || map.tile[y1][x1] == 3 || map.tile[y1][x1] == 6 || map.tile[y1][x1] == 7 || map.tile[y2][x1] == 1 || map.tile[y2][x1] == 2 || map.tile[y2][x1] == 3 || map.tile[y2][x1] == 6 || map.tile[y2][x1] == 7) {
+				x_pos = (x1 + 1) * TILE_SIZE;
+				x_change = 0;
+			}
+		}
+	}
+	//kiểm tra theo chiều dọc
+	int widthtMin = (w_frame < TILE_SIZE ? w_frame : TILE_SIZE) - 30;//  lấy chiều rộng bằng chân nhân vật
+	x1 = (x_pos + 1) / TILE_SIZE;
+	x2 = (x_pos + widthtMin) / TILE_SIZE;
+	y1 = (y_pos + y_change) / TILE_SIZE;
+	y2 = (y_pos + y_change + h_frame - 1) / TILE_SIZE;
+	if (x1 >= 0 && x2 <= MAX_MAP_WIDTH && y1 >= 0 && y2 <= MAX_MAP_HEIGHT) {
+		if (y_change > 0) {
+			if (map.tile[y2][x1] == 1 || map.tile[y2][x1] == 2 || map.tile[y2][x1] == 3 || map.tile[y2][x1] == 6 || map.tile[y2][x1] == 7 || map.tile[y2][x2] == 1 || map.tile[y2][x2] == 2 || map.tile[y2][x2] == 3 || map.tile[y2][x2] == 6 || map.tile[y2][x2] == 7) {
+				y_pos = y2 * TILE_SIZE;
+				y_pos -= (h_frame + 1);
+				y_change = 0;
+				onGround = true;
+			}
+		}
+		else if (y_change < 0) {
+			if (map.tile[y1][x1] == 1 || map.tile[y1][x1] == 2 || map.tile[y1][x1] == 3 || map.tile[y1][x1] == 6 || map.tile[y1][x1] == 7 || map.tile[y1][x2] == 1 || map.tile[y1][x2] == 2 || map.tile[y1][x2] == 3 || map.tile[y1][x2] == 6 || map.tile[y1][x2] == 7) {
+				y_pos = (y1 + 1) * TILE_SIZE;
+				y_change = 0;
+			}
+		}
+	}
+	x_pos += x_change;
+	y_pos += y_change;
+	if (x_pos < 0) x_pos = 0;
+	if (x_pos + w_frame > map.x_max) x_pos = map.x_max - w_frame - 1;
+	if (y_pos < 0) y_pos = 0;
+	if (y_pos + h_frame >= (map.y_max - 64)) {
+		onGround = true;
+		y_pos = map.y_max - h_frame - 64;
+	}
+}
+
+void Monster::HanleMoveType(SDL_Renderer* des ){
+	if (type_move == MONSTER) {
+		;
+	}
+	else {
+		if (onGround) {
+			if (x_pos > move_limit_b) {
+				input_type.left = 1;
+				input_type.right = 0;
+				LoadImg("image/alien_fly_left.png", des);
+			}
+			else if (x_pos < move_limit_a) {
+				input_type.left = 0;
+				input_type.right = 1;
+				LoadImg("image/alien_fly_right.png", des);
+			}
+		}
+		else{
+			if (input_type.left == 1) {
+				LoadImg("image/alien_fly_left.png", des);
+			}
+		}
+	}
+
+}
+
---
+++ Monster.h
@@ -0,0 +1,66 @@
+﻿#ifndef MONSTER_H_
+#define MONSTER_H_
+#include "BaseFunction.h"
+#include "BaseObject.h"
+#include "Map.h"
+#define MONSTER_SPEED 1
+#define MONSTER_FALLING_SPEED 1
+#define MAX_MONSTER_FALLING_SPEED 2
+#define MONSTER_SPEED 0.4
+
+class Monster : public BaseObject {
+public:
+	Monster();
+	~Monster();
+	void SetXchange(const int& x) { x_change = x; }
+	void SetYchange(const int& y) { y_change = y; }
+	int GetXchange() const { return x_change; }
+	int GetYchange() const { return y_change; }
+	void SetXpos(const int& x) { x_pos = x; }
+	void SetYpos(const int& y) { y_pos = y; }
+	int GetXpos() const { return x_pos; }
+	int GetYpos() const { return y_pos; }
+	int GetWframe() const { return w_frame; }
+	int GetHframe() const { return h_frame; }
+	void SetMapXY(const int& x_start_, const int& y_start_) {
+		map_x = x_start_;
+		map_y = y_start_;
+	}
+	void LoadFrame();
+	virtual bool LoadImg(string path, SDL_Renderer* des);
+	void Show(SDL_Renderer* des);
+	void Update(Map& map);
+	void MapCollisionCheck(Map& map);// hàm kiểm tra va chạm bản đồ
+	void SetTypeMove(const int& typeMove_) { type_move = typeMove_; }
+	void SetMovePos(const int& posa, const int& posb) {
+		move_limit_a = posa;
+		move_limit_b = posb;
+	}
+	void SetInputLeft(const int& inputLeft_) { input_type.left = inputLeft_; }
+	void HanleMoveType(SDL_Renderer* des);
+	enum TypeMove {
+		MONSTER = 0,
+		MOVE_MONSTER = 1,
+	};
+
+private:
+	double x_change;
+	double y_change;
+	double x_pos;
+	double y_pos;
+	int w_frame;
+	int h_frame;
+	SDL_Rect frame_clip[5];
+	int frame;
+	bool onGround;
+	int map_x;
+	int map_y;
+	int type_move;
+	KeyPress input_type;
+	int move_limit_a;
+	int move_limit_b;
+};
+
+
+#endif // !MONSTER_H_
+
